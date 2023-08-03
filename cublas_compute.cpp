a369359f7739884a75421885ba4e402658336796
maple_ma <maple.ma@yuansuan.com>
2023-08-03 13:35:25 +0800

    add cublas circle time

--- cublas_compute.cpp
+++ cublas_compute.cpp
@@ -4,59 +4,66 @@
 #include "operation.h"
 #include <iostream>
 
-void cublas_multiply(const std::vector<datatype> &matrix1, const std::vector<datatype> &matrix2,int num)
+void cublas_multiply(const std::vector<datatype> &matrix1, const std::vector<datatype> &matrix2, int num)
 {
-    struct timeval tc1, tc2;
-    double timeusec;
-    gettimeofday(&tc1, NULL);
-    
-    // Initialize cuBLAS
-    cublasHandle_t handle;
-    cublasCreate(&handle);
+    double average_time = 0.0;
+    for (int i = 0; i < circle_time; i++)
+    {
+        struct timeval tc1, tc2;
+        double timeusec;
+        gettimeofday(&tc1, NULL);
 
-    // Allocate memory on the GPU
-    datatype *d_A, *d_B, *d_C;
-    datatype *mat1, *mat2, *result;
-    cudaMalloc((void **)&d_A, num * num * sizeof(datatype));
-    cudaMalloc((void **)&d_B, num * num * sizeof(datatype));
-    cudaMalloc((void **)&d_C, num * num * sizeof(datatype));
-    result = (datatype *)malloc(num * num * sizeof(datatype));
-    mat1 = (datatype *)malloc(num * num * sizeof(datatype));
-    mat2 = (datatype *)malloc(num * num * sizeof(datatype));
-    std::copy(matrix1.begin(), matrix1.end(), mat1);
-    std::copy(matrix2.begin(), matrix2.end(), mat2);
+        // Initialize cuBLAS
+        cublasHandle_t handle;
+        cublasCreate(&handle);
 
-    // Copy input matrices from CPU to GPU
-    cudaMemcpy(d_A, mat1, num * num * sizeof(datatype), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, mat2, num * num * sizeof(datatype), cudaMemcpyHostToDevice);
+        // Allocate memory on the GPU
+        datatype *d_A, *d_B, *d_C;
+        datatype *mat1, *mat2, *result;
+        cudaMalloc((void **)&d_A, num * num * sizeof(datatype));
+        cudaMalloc((void **)&d_B, num * num * sizeof(datatype));
+        cudaMalloc((void **)&d_C, num * num * sizeof(datatype));
+        result = (datatype *)malloc(num * num * sizeof(datatype));
+        mat1 = (datatype *)malloc(num * num * sizeof(datatype));
+        mat2 = (datatype *)malloc(num * num * sizeof(datatype));
+        std::copy(matrix1.begin(), matrix1.end(), mat1);
+        std::copy(matrix2.begin(), matrix2.end(), mat2);
 
-    const datatype alpha = 1.0;
-    const datatype beta = 0.0;
+        // Copy input matrices from CPU to GPU
+        cudaMemcpy(d_A, mat1, num * num * sizeof(datatype), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_B, mat2, num * num * sizeof(datatype), cudaMemcpyHostToDevice);
 
-    // Perform matrix multiplication using cuBLAS
-    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                num, num, num, &alpha,
-                d_B, num, d_A, num, &beta, d_C, num);
+        const datatype alpha = 1.0;
+        const datatype beta = 0.0;
 
-    // Copy the result back from GPU to the global variable
-    cudaMemcpy(result, d_C, num * num * sizeof(datatype), cudaMemcpyDeviceToHost);
+        // Perform matrix multiplication using cuBLAS
+        cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+                    num, num, num, &alpha,
+                    d_B, num, d_A, num, &beta, d_C, num);
 
-    // Free GPU memory and destroy cuBLAS handle
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-    free(result);
-    free(mat1);
-    free(mat2);
-    
-    cublasDestroy(handle);
-    
-    result=NULL;
-    mat1=NULL;
-    mat2=NULL;
-    
-    gettimeofday(&tc2, NULL);
-    timeusec = (tc2.tv_sec - tc1.tv_sec) + (double)(tc2.tv_usec - tc1.tv_usec) / 1000000.0;
-    times[num / gap - 1] = timeusec;
-    std::cout << "compute time :" << timeusec << "s" << std::endl;
+        // Copy the result back from GPU to the global variable
+        cudaMemcpy(result, d_C, num * num * sizeof(datatype), cudaMemcpyDeviceToHost);
+
+        // Free GPU memory and destroy cuBLAS handle
+        cudaFree(d_A);
+        cudaFree(d_B);
+        cudaFree(d_C);
+        free(result);
+        free(mat1);
+        free(mat2);
+
+        cublasDestroy(handle);
+
+        result = NULL;
+        mat1 = NULL;
+        mat2 = NULL;
+
+        gettimeofday(&tc2, NULL);
+        timeusec = (tc2.tv_sec - tc1.tv_sec) + (double)(tc2.tv_usec - tc1.tv_usec) / 1000000.0;
+        average_time += timeusec / circle_time;
+
+        std::cout << "compute time :" << timeusec << "s" << std::endl;
+    }
+    times[num / gap - 1] = average_time;
+    std::cout << "average time :" << average_time << "s" << std::endl;
 }
